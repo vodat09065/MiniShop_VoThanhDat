215ba0e94855d5d10761ff6b408269af3ac70a18
VoThanhDat96 <datthanhvo19@gmail.com>
2026-08-20 08:24:35 +0700

    Add cache buster to master.php to fix JS caching issue

--- views/client/layouts/master.php
+++ views/client/layouts/master.php
@@ -6,7 +6,7 @@
 <title><?= $title ?? "Mini Shop" ?></title>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css" rel="stylesheet">
-<link href="<?= BASE_URL ?>assets/client/style.css" rel="stylesheet">
+<link href="<?= BASE_URL ?>assets/client/style.css?v=<?= time() ?>" rel="stylesheet">
 </head>
 <body class="d-flex flex-column min-vh-100">
 <?php include __DIR__ . "/header.php"; ?>
@@ -16,6 +16,6 @@
 <?php include __DIR__ . "/footer.php"; ?>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 <script>const BASE_URL = "<?= BASE_URL ?>";</script>
-<script src="<?= BASE_URL ?>assets/client/cart.js"></script>
+<script src="<?= BASE_URL ?>assets/client/cart.js?v=<?= time() ?>"></script>
 </body>
 </html>
